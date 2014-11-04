bd527298c7357ffdf992d52891a6fe87a2d8a936
Alexander Barker <alex@1stleg.com>
2014-11-04 00:22:21 -0800

    Fixed a strange issue with a jvm crash related to this function.  I don't believe is being used anymore if it was ever used correctly.

--- src/logger.c
+++ src/logger.c
@@ -67,28 +67,3 @@ UIOHOOK_API void hook_set_logger_proc(logger_t logger_proc) {
 		logger = logger_proc;
 	}
 }
-
-//TODO: think about where does this make more sense
-//Whether the event should be trapped/consumed or not
-//with extern doesnt compile on mingw but without doesn't on mac-clang...
-//extern bool trapEvents[ EVENT_MOUSE_WHEEL - EVENT_HOOK_START + 1];
-bool trapEvents[ EVENT_MOUSE_WHEEL - EVENT_HOOK_START + 1];
-
-//TODO: move to a better place and initialize somewhere to all false
-UIOHOOK_API void hook_trap_event( event_type eventType, bool shouldBeTrapped ){
-	trapEvents[ eventType - 1 ] = shouldBeTrapped;
-}
-
-//TODO: move to a better place and initialize somewhere to all false
-void reserveIfTrapEvent( uint16_t *reserveEvent, event_type eventType ){
-	//Set reserved if the event was marked to be trapped/consumed
-	//And respect the value if already enable
-	//This can be controlled by the application using the library api
-	//i.e. using a special key comb to enable/disable traps
-	if( *reserveEvent ^ 0x01 )
-		*reserveEvent = (trapEvents[ eventType - 1 ]) ? 0x01: 0x00;
-		//event.reserved = (trapEvents[ event.type - 1 ]) ? 0x01: 0x00;
-	//return (trapEvents[ eventType - 1 ]) ? 0x01: 0x00;
-}
-
-

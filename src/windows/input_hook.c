503b363d79d51619a4d78a41c4211e2f462c110a
Alexander Barker <alex@1stleg.com>
2015-04-21 00:30:23 -0700

    Fixed a bug I introduced on Windows.

--- src/windows/input_hook.c
+++ src/windows/input_hook.c
@@ -94,20 +94,21 @@ static inline unsigned short int get_modifiers() {
 static void initialize_modifiers() {
 	current_modifiers = 0x0000;
 
-	if (GetKeyState(VK_LSHIFT)	 > 0)	{ set_modifier_mask(MASK_SHIFT_L);	}
-	if (GetKeyState(VK_RSHIFT)   > 0)	{ set_modifier_mask(MASK_SHIFT_R);	}
-	if (GetKeyState(VK_LCONTROL) > 0)	{ set_modifier_mask(MASK_CTRL_L);	}
-	if (GetKeyState(VK_RCONTROL) > 0)	{ set_modifier_mask(MASK_CTRL_R);	}
-	if (GetKeyState(VK_LMENU)    > 0)	{ set_modifier_mask(MASK_ALT_L);	}
-	if (GetKeyState(VK_RMENU)    > 0)	{ set_modifier_mask(MASK_ALT_R);	}
-	if (GetKeyState(VK_LWIN)     > 0)	{ set_modifier_mask(MASK_META_L);	}
-	if (GetKeyState(VK_RWIN)     > 0)	{ set_modifier_mask(MASK_META_R);	}
+	// NOTE We are checking the high order bit, so it will be < 0 for a singed short.
+	if (GetKeyState(VK_LSHIFT)	 < 0)	{ set_modifier_mask(MASK_SHIFT_L);	}
+	if (GetKeyState(VK_RSHIFT)   < 0)	{ set_modifier_mask(MASK_SHIFT_R);	}
+	if (GetKeyState(VK_LCONTROL) < 0)	{ set_modifier_mask(MASK_CTRL_L);	}
+	if (GetKeyState(VK_RCONTROL) < 0)	{ set_modifier_mask(MASK_CTRL_R);	}
+	if (GetKeyState(VK_LMENU)    < 0)	{ set_modifier_mask(MASK_ALT_L);	}
+	if (GetKeyState(VK_RMENU)    < 0)	{ set_modifier_mask(MASK_ALT_R);	}
+	if (GetKeyState(VK_LWIN)     < 0)	{ set_modifier_mask(MASK_META_L);	}
+	if (GetKeyState(VK_RWIN)     < 0)	{ set_modifier_mask(MASK_META_R);	}
 
-	if (GetKeyState(VK_LBUTTON)	 > 0)	{ set_modifier_mask(MASK_BUTTON1);	}
-	if (GetKeyState(VK_RBUTTON)  > 0)	{ set_modifier_mask(MASK_BUTTON2);	}
-	if (GetKeyState(VK_MBUTTON)  > 0)	{ set_modifier_mask(MASK_BUTTON3);	}
-	if (GetKeyState(VK_XBUTTON1) > 0)	{ set_modifier_mask(MASK_BUTTON4);	}
-	if (GetKeyState(VK_XBUTTON2) > 0)	{ set_modifier_mask(MASK_BUTTON5);	}
+	if (GetKeyState(VK_LBUTTON)	 < 0)	{ set_modifier_mask(MASK_BUTTON1);	}
+	if (GetKeyState(VK_RBUTTON)  < 0)	{ set_modifier_mask(MASK_BUTTON2);	}
+	if (GetKeyState(VK_MBUTTON)  < 0)	{ set_modifier_mask(MASK_BUTTON3);	}
+	if (GetKeyState(VK_XBUTTON1) < 0)	{ set_modifier_mask(MASK_BUTTON4);	}
+	if (GetKeyState(VK_XBUTTON2) < 0)	{ set_modifier_mask(MASK_BUTTON5);	}
 
 	// FIXME Add check for lock masks!
 }

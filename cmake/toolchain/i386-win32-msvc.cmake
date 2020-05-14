54bb180455ab558c3002816784275fccc3022120
Alexander Barker <alex@1stleg.com>
2020-05-13 19:09:45 -0700

    Cleaning up build workflow

--- cmake/toolchain/i386-win32-msvc.cmake
+++ cmake/toolchain/i386-win32-msvc.cmake
@@ -1,7 +1,22 @@
-# the name of the target operating system
+# libUIOHook: Cross-platform userland keyboard and mouse hooking.
+# Copyright (C) 2006-2020 Alexander Barker.  All Rights Received.
+# https://github.com/kwhat/libuiohook/
+#
+# libUIOHook is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# libUIOHook is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_BUILD_TYPE RelWithDebInfo)
 
-# which compilers to use for C
 set(CMAKE_C_COMPILER cl.exe)
 set(CMAKE_C_FLAGS "/arch:IA32")
--- cmake/toolchain/x86_64-linux-gnu.cmake
+++ cmake/toolchain/x86_64-linux-gnu.cmake
@@ -1,19 +1,31 @@
-# the name of the target operating system
+# libUIOHook: Cross-platform userland keyboard and mouse hooking.
+# Copyright (C) 2006-2020 Alexander Barker.  All Rights Received.
+# https://github.com/kwhat/libuiohook/
+#
+# libUIOHook is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# libUIOHook is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_BUILD_TYPE RelWithDebInfo)
 
-# which compilers to use for C
 set(CMAKE_C_COMPILER x86_64-linux-gnu-gcc)
-set(CMAKE_C_FLAGS "-march=x86-64 -mtune=generic -fomit-frame-pointer -flto -ffat-lto-objects -fno-stack-protector -pipe")
+set(CMAKE_C_FLAGS "-march=x86-64 -mtune=generic -fomit-frame-pointer -flto -fno-stack-protector -pipe")
 
 set(CMAKE_EXE_LINKER_FLAGS "-fuse-ld=gold -Wl,-O1 -Wl,--as-needed")
 set(CMAKE_SHARED_LINKER_FLAGS "-fuse-ld=gold -Wl,-O1 -Wl,--as-needed")
 
-set(THREADS_PTHREAD_ARG "2" CACHE STRING "Forcibly set by CMakeLists.txt." FORCE)
-
+# ubuntu stuff...
 set(CMAKE_FIND_ROOT_PATH /usr/x86_64-linux-gnu)
-
-# the target environment lib locations
 set(CMAKE_LIBRARY_PATH /usr/lib/x86_64-linux-gnu/;/usr/lib64/)
 
 # adjust the default behaviour of the FIND_XXX() commands:

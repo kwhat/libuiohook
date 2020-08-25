a29b0adfcd6d3398f7bd8a21294214bf9742a95f
Alexander Barker <alex@1stleg.com>
2020-08-25 09:31:55 -0700

    Rewrite all of CMakeLists.txt to try and make things simpler

--- src/windows/post_event.c
+++ src/windows/post_event.c
@@ -1,13 +1,13 @@
-/* libUIOHook: Cross-platform userland keyboard and mouse hooking.
+/* UIOHook: Cross-platform userland keyboard and mouse hooking.
  * Copyright (C) 2006-2020 Alexander Barker.  All Rights Received.
- * https://github.com/kwhat/libuiohook/
+ * https://github.com/kwhat/uiohook/
  *
- * libUIOHook is free software: you can redistribute it and/or modify
+ * UIOHook is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * libUIOHook is distributed in the hope that it will be useful,
+ * UIOHook is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -15,10 +15,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
 
 #include <stdio.h>
 #include <uiohook.h>

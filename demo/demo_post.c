06a767a56a5b0d466f56fdecd86de7ee2cdf7bac
Alexander Barker <alex@1stleg.com>
2022-02-18 08:43:11 -0800

    Posting events on macOS, Windows and X11 produce different results for the same event chains. Fixes #111, #112

--- demo/demo_post.c
+++ demo/demo_post.c
@@ -26,30 +26,109 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#define sleep(x) Sleep(1000 * (x))
+#else
+#include <unistd.h>
 #endif
 
 // Virtual event pointers
 static uiohook_event *event = NULL;
 
+
+bool logger_proc(unsigned int level, const char *format, ...) {
+    bool status = false;
+
+    va_list args;
+    switch (level) {
+        case LOG_LEVEL_INFO:
+            va_start(args, format);
+            status = vfprintf(stdout, format, args) >= 0;
+            va_end(args);
+            break;
+
+        case LOG_LEVEL_WARN:
+        case LOG_LEVEL_ERROR:
+            va_start(args, format);
+            status = vfprintf(stderr, format, args) >= 0;
+            va_end(args);
+            break;
+    }
+
+    return status;
+}
+
 // TODO Implement CLI options.
 //int main(int argc, char *argv[]) {
 int main() {
+    // Set the logger callback for library output.
+    hook_set_logger_proc(&logger_proc);
+
     // Allocate memory for the virtual events only once.
     event = (uiohook_event *) malloc(sizeof(uiohook_event));
+    if (event == NULL) {
+        return UIOHOOK_ERROR_OUT_OF_MEMORY;
+    }
+
+    sleep(1);
+
+    event->type = EVENT_MOUSE_PRESSED;
+    event->data.mouse.button = MOUSE_BUTTON1;
+    #ifdef _WIN32
+    event->data.mouse.x = 4;
+    event->data.mouse.y = 4;
+    #else
+    event->data.mouse.x = 10;
+    event->data.mouse.y = 35;
+    #endif
+    hook_post_event(event);
+
+    sleep(1);
+
+    for (int i = 0; i < 275; i++) {
+        event->type = EVENT_MOUSE_MOVED;
+        event->data.mouse.button = MOUSE_NOBUTTON;
+        event->data.mouse.x = i;
+        event->data.mouse.y = i;
+        hook_post_event(event);
+    }
+
+    sleep(1);
+
+    event->type = EVENT_MOUSE_RELEASED;
+    event->data.mouse.button = MOUSE_BUTTON1;
+    hook_post_event(event);
+
+    sleep(1);
 
     event->type = EVENT_KEY_PRESSED;
     event->mask = 0x00;
+    event->data.keyboard.keychar = CHAR_UNDEFINED;
+    event->data.keyboard.keycode = VC_ESCAPE;
+    hook_post_event(event);
 
-    event->data.keyboard.keycode = VC_A;
+
+    /*
+    event->type = EVENT_KEY_PRESSED;
+    event->mask = 0x00;
     event->data.keyboard.keychar = CHAR_UNDEFINED;
 
+    event->data.keyboard.keycode = VC_SHIFT_L;
     hook_post_event(event);
+
+    event->data.keyboard.keycode = VC_A;
+    hook_post_event(event);
+
 
     event->type = EVENT_KEY_RELEASED;
 
+    event->data.keyboard.keycode = VC_A;
     hook_post_event(event);
+
+    event->data.keyboard.keycode = VC_SHIFT_L;
+    hook_post_event(event);
+    */
 
     free(event);
 
-    return 0;
+    return UIOHOOK_SUCCESS;
 }

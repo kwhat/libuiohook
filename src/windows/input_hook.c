486c718cbefccfeaed59bca187532c4f064b6e1e
Alexander Barker <alex@1stleg.com>
2022-03-15 15:37:25 -0700

    Fixed horizontal scroll sign (direction) on Windows. Fixed button release/click events for scrolling on X11.
    Addresses https://github.com/kwhat/jnativehook/issues/190

--- src/windows/input_hook.c
+++ src/windows/input_hook.c
@@ -484,7 +484,12 @@ static void process_mouse_wheel(MSLLHOOKSTRUCT *mshook, uint8_t direction) {
      * forward, away from the user; a negative value indicates that
      * the wheel was rotated backward, toward the user. One wheel
      * click is defined as WHEEL_DELTA, which is 120. */
-    event.data.wheel.rotation = ((int16_t) HIWORD(mshook->mouseData) / WHEEL_DELTA) * -1;
+    event.data.wheel.rotation = (int16_t) HIWORD(mshook->mouseData) / WHEEL_DELTA;
+
+    // Vertical direction needs to be inverted on Windows to conform with other platforms.
+    if (direction == WHEEL_VERTICAL_DIRECTION) {
+        event.data.wheel.rotation *= -1;
+    }
 
     // Set the direction based on what event was received.
     event.data.wheel.direction = direction;

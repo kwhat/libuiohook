22ba8a034eaa6cf8f6a5665dc2c239cfc6183480
Alexander Barker <alex@1stleg.com>
2022-04-24 11:31:25 -0700

    Refactoring dispatcher for Windows.

--- src/windows/dispatch_event.h
+++ src/windows/dispatch_event.h
@@ -15,3 +15,22 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <stdbool.h>
+#include <windows.h>
+
+extern bool dispatch_hook_enable();
+
+extern bool dispatch_hook_disable();
+
+extern bool dispatch_key_press(KBDLLHOOKSTRUCT *kbhook);
+
+extern bool dispatch_key_release(KBDLLHOOKSTRUCT *kbhook);
+
+extern bool dispatch_button_press(MSLLHOOKSTRUCT *mshook, uint16_t button);
+
+extern bool dispatch_button_release(MSLLHOOKSTRUCT *mshook, uint16_t button);
+
+extern bool dispatch_mouse_move(MSLLHOOKSTRUCT *mshook);
+
+extern bool dispatch_mouse_wheel(MSLLHOOKSTRUCT *mshook, uint8_t direction);

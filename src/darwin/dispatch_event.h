1104fd1ba83be3b2f79285123a937121c4182925
Alexander Barker <alex@1stleg.com>
2022-04-26 08:34:17 -0700

    Refactoring dispatcher for Darwin.

--- src/darwin/dispatch_event.h
+++ src/darwin/dispatch_event.h
@@ -15,3 +15,29 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <ApplicationServices/ApplicationServices.h>
+
+extern bool dispatch_hook_enabled(uint64_t timestamp);
+
+extern bool dispatch_hook_disabled(uint64_t timestamp);
+
+extern bool dispatch_key_press(uint64_t timestamp, CGEventRef event_ref);
+
+extern bool dispatch_key_release(uint64_t timestamp, CGEventRef event_ref);
+
+/* These events are totally undocumented for the CGEvent type, but are required to grab media and caps-lock keys. */
+extern bool dispatch_system_key(uint64_t timestamp, CGEventRef event_ref);
+
+extern bool dispatch_modifier_change(uint64_t timestamp, CGEventRef event_ref);
+
+extern bool dispatch_button_press(uint64_t timestamp, CGEventRef event_ref, uint16_t button);
+
+extern bool dispatch_button_release(uint64_t timestamp, CGEventRef event_ref, uint16_t button);
+
+extern bool dispatch_mouse_move(uint64_t timestamp, CGEventRef event_ref);
+
+extern bool dispatch_mouse_wheel(uint64_t timestamp, CGEventRef event_ref);

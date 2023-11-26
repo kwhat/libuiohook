edeb4ea1132c586d3a30994415dff34114b772cf
Alexander Barker <alex@1stleg.com>
2023-11-26 07:28:40 -0800

    Adding support for relative mouse movement (Bug #91)

--- src/darwin/dispatch_event.c
+++ src/darwin/dispatch_event.c
@@ -559,6 +559,12 @@ bool dispatch_mouse_move(uint64_t timestamp, CGEventRef event_ref) {
 
     CGPoint event_point = CGEventGetLocation(event_ref);
 
+
+    logger(LOG_LEVEL_WARN, "#### %s [%u]: Mouse %s to %i, %i.\n",
+            __FUNCTION__, __LINE__, is_mouse_dragged() ? "dragged" : "moved",
+            CGEventGetIntegerValueField(event_ref, kCGMouseEventDeltaX),
+            CGEventGetIntegerValueField(event_ref, kCGMouseEventDeltaY));
+    
     // Populate mouse motion event.
     uio_event.time = timestamp;
     if (is_mouse_dragged()) {
